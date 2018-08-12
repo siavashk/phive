4183c4f5e697578f49b3c7bedcb08e334dffd651
Siavash Khallaghi <siavashk@ece.ubc.ca>
2018-08-11 23:57:08 -0700

    can draw a single triangle

--- executables/phivegui/main.cpp
+++ executables/phivegui/main.cpp
@@ -1,28 +1,76 @@
 #include "SDL.h"
 #include <stdio.h>
+#include <iostream>
+
+// init ... The init function, it calls the SDL init function.
+int init()
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        return -1;
+    }
+    
+    return 0;
+}
+
+// draw ... Draw a series of lines to the screen to create a triangle.
+void draw(SDL_Renderer* Renderer) {
+    //Set the draw color...
+    SDL_SetRenderDrawColor(Renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    //Clear the screen.
+    SDL_RenderClear(Renderer);
+    
+    //Setting the actual draw color.
+    SDL_SetRenderDrawColor(Renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+    
+    //Drawing the lines we want. (-1, 0), (0, 1), (1, 0)
+    SDL_RenderDrawLine(Renderer, 0, 240, 320, 0);
+    SDL_RenderDrawLine(Renderer, 640, 240, 320, 0);
+    SDL_RenderDrawLine(Renderer, 0, 240, 640, 240);
+    
+    //Update the Renderer.
+    SDL_RenderPresent(Renderer);
+}
+
+// createWindowContext ... Creating the window for later use in rendering and stuff.
+SDL_Window* createWindowContext(std::string title) {
+    //Declaring the variable the return later.
+    SDL_Window* Window = NULL;
+    
+    //Creating the window and passing that reference to the previously declared variable.
+    Window = SDL_CreateWindow("Triangle World", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+    
+    //Returning the newly creted Window context.
+    return Window;
+}
+
+// createRenderContext ... Creating the rendering context so that I can render things to the Window.
+SDL_Renderer* createRendererContext(SDL_Window* Window) {
+    //Declaring the variable for the Renderer context.
+    SDL_Renderer* Renderer = NULL;
+    
+    //Create the Renderer with the window.
+    Renderer = SDL_CreateRenderer(Window, -1, 0);
+    
+    //Return the Renderer.
+    return Renderer;
+}
+
 
 int main(int argc, char* argv[]) {
     
-    SDL_Window *window;                    // Declare a pointer
+    //Calling the SDL init stuff.
+    init();
     
-    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+    //Creating the context for SDL2.
+    SDL_Window* window = createWindowContext("Hello World!");
     
-    // Create an application window with the following settings:
-    window = SDL_CreateWindow(
-                              "An SDL2 window",                  // window title
-                              SDL_WINDOWPOS_UNDEFINED,           // initial x position
-                              SDL_WINDOWPOS_UNDEFINED,           // initial y position
-                              640,                               // width, in pixels
-                              480,                               // height, in pixels
-                              SDL_WINDOW_OPENGL                  // flags - see below
-                              );
+    //Creating the rendering context.
+    SDL_Renderer* renderer = createRendererContext(window);
     
-    // Check that the window was successfully created
-    if (window == NULL) {
-        // In the case that the window could not be made...
-        printf("Could not create window: %s\n", SDL_GetError());
-        return 1;
-    }
+    //Drawing!
+    draw(renderer);
+
     
     // A basic main loop to prevent blocking
     bool is_running = true;
